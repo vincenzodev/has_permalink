8a8f6a7c62a4ae46a8f47b8229edaf36dfbc1a42
vincenzodev <vincenzo.farr@gmail.com>
2012-04-11 12:15:34 +0200

    Globalize3 support

--- lib/has_permalink.rb
+++ lib/has_permalink.rb
@@ -35,12 +35,12 @@ module HasPermalink
 
     # Generate permalink for the instance if the permalink is empty or nil.
     def generate_permalink
-      self.permalink = fix_duplication(normalize(self.send(generate_from))) if permalink.blank?
+      self[:permalink] = fix_duplication(normalize(self.send(generate_from))) if permalink.blank?
     end
 
     # Generate permalink for the instance and overwrite any existing value.
     def generate_permalink!
-      self.permalink = fix_duplication(normalize(self.send(generate_from)))
+      self[:permalink] = fix_duplication(normalize(self.send(generate_from)))
     end
 
     # Override to send permalink as params[:id]
